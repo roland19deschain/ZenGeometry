9fefe990b1bc588d5c9c3e1e366ab82f3f7bd82e
Alexey Roik <alexey.roik@nebulagames.org>
2020-05-01 21:26:02 +0300

    Add empty project

---
+++ ZenGeometry/ZenGeometry.h
@@ -0,0 +1,19 @@
+//
+//  ZenGeometry.h
+//  ZenGeometry
+//
+//  Created by Alexey Roik on 5/1/20.
+//  Copyright © 2020 Nebula. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZenGeometry.
+FOUNDATION_EXPORT double ZenGeometryVersionNumber;
+
+//! Project version string for ZenGeometry.
+FOUNDATION_EXPORT const unsigned char ZenGeometryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZenGeometry/PublicHeader.h>
+
+
