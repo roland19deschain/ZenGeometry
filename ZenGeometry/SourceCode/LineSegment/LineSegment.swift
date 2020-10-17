424b59f6a062cf568cb6d62d2746018aec4aea48
Alexey Roik <alexey.roik@nebulagames.org>
2020-10-17 21:52:12 +0300

    Fix 'LineSegment' initializer protection level

--- ZenGeometry/SourceCode/LineSegment/LineSegment.swift
+++ ZenGeometry/SourceCode/LineSegment/LineSegment.swift
@@ -1,6 +1,20 @@
 import CoreGraphics
 
 public struct LineSegment {
+	
+	// MARK: - Stored Properties
+	
 	let start: CGPoint
 	let end: CGPoint
+	
+	// MARK: - Life Cycle
+	
+	public init(
+		start: CGPoint,
+		end: CGPoint
+	) {
+		self.start = start
+		self.end = end
+	}
+	
 }
